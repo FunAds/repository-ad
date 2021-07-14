e299d8f5e61f6a63236c507e5c070d453d801bb4
FunAds <87407238+FunAds@users.noreply.github.com>
2021-07-14 09:20:21 +0500

    Add files via upload

---
+++ precache-manifest.ef4435485e38e86368f8242759c015cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161bf5b100465896c4e3803696bcea29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f62ecd2b5b704d426bc9",
+    "url": "/static/css/2.2d6169ee.chunk.css"
+  },
+  {
+    "revision": "e485130190e34d1ca4bf",
+    "url": "/static/css/main.272a54e3.chunk.css"
+  },
+  {
+    "revision": "f62ecd2b5b704d426bc9",
+    "url": "/static/js/2.95af1eb9.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.95af1eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e485130190e34d1ca4bf",
+    "url": "/static/js/main.fb734402.chunk.js"
+  },
+  {
+    "revision": "d5d50483e52d53a4b219",
+    "url": "/static/js/runtime-main.29d8161a.js"
+  }
+]);
